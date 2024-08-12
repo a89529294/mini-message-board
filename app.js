10e40664ba563e73656cec5d81153d680d3c09a7
Albert Chang <a89529294@hotmail.com>
2024-08-12 09:12:04 +0800

    init

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [
+  {
+    id: crypto.randomUUID(),
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    id: crypto.randomUUID(),
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  messages.push({
+    id: crypto.randomUUID(),
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+app.get("/messages/:id", (req, res) => {
+  const message = messages.find((message) => message.id === req.params.id);
+  res.render("message", { message });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
